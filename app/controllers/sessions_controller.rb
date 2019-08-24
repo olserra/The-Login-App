4186a03d38584088c76fabf5ed6125d3c82f9fd8
olserra <olserra@gmail.com>
2019-08-24 15:50:40 +0100

    boraaa

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,37 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password])
+    user = User.find_by(email: params[:email])    
+    if user&.authenticate(params[:password])      
       session[:user_id] = user.id
-      redirect_to posts_url, notice: 'Logged in!'
-    else
-      flash.now.alert = 'Email or password is invalid'
+      user.update(login_count: 0)
+      user.update(locked: false)
+      flash.now.alert = 'Logged in'
       render :new
-    end
+    else            
+      if user 
+        session.delete(:user_id)
+        if user.locked
+          flash.now.alert = 'User locked, contact the admin'
+          render :new
+        end
+        user.login_count = (user.login_count || 1) + 1
+        user.save
+        if user.login_count >= 3
+          user.update(locked: true)
+          flash.now.alert = 'User locked'          
+        else
+          flash.now.alert = 'Email or password is invalid'            
+        end                
+      else
+        flash.now.alert = 'Email or password is invalid'
+        render :new
+      end      
+    end    
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to posts_url, notice: 'Logged out!'
+    redirect_to '/', notice: 'Logged out!'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect('/session/new')
   resources :users, only: %i[new create]
   resource :session, only: %i[new create destroy]
 
