6ce8f60d856445d71ed5ff05fa19975ebab98615
olserra <olserra@gmail.com>
2019-08-24 14:46:53 +0100

    boraaa

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :session, only: %i[new create destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
